805a52b2445249389f6ae40367d72a239bbc445d
Ryan Apellanes <ryanapellanes.dev@gmail.com>
2022-04-08 22:11:39 -0700

    extra 1,2,3

--- src/exercise/02.extra-1.js
+++ src/exercise/02.extra-1.js
@@ -3,20 +3,14 @@
 
 import * as React from 'react'
 
-function useLocalStorageState(key, defaultValue = '') {
-  const [state, setState] = React.useState(
-    () => window.localStorage.getItem(key) ?? defaultValue,
+function Greeting({initialName = ''}) {
+  const [name, setName] = React.useState(
+    () => window.localStorage.getItem('name') ?? initialName,
   )
 
   React.useEffect(() => {
-    window.localStorage.setItem(key, state)
-  }, [key, state])
-
-  return [state, setState]
-}
-
-function Greeting({initialName = ''}) {
-  const [name, setName] = useLocalStorageState('name', initialName)
+    window.localStorage.setItem('name', name)
+  })
 
   function handleChange(event) {
     setName(event.target.value)
