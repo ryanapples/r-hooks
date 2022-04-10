95fddd64c102176ea54d138d4cd46b2931fbb774
Ryan Apellanes <ryanapellanes.dev@gmail.com>
2022-04-10 15:04:04 -0700

    extra 2: using a status

--- src/exercise/06.extra-1.js
+++ src/exercise/06.extra-1.js
@@ -10,42 +10,41 @@ import {
 } from '../pokemon'
 
 function PokemonInfo({pokemonName}) {
-  const [status, setStatus] = React.useState('idle')
   const [pokemon, setPokemon] = React.useState(null)
   const [error, setError] = React.useState(null)
 
   React.useEffect(() => {
     if (!pokemonName) return
-    setStatus('pending')
+    setPokemon(null)
+
     async function whosThatPokemon() {
       try {
         const result = await fetchPokemon(pokemonName)
         setPokemon(result)
-        setStatus('resolved')
       } catch (error) {
         setError(error)
-        setStatus('rejected')
       }
     }
+
     whosThatPokemon()
   }, [pokemonName])
 
-  if (status === 'idle') {
-    return 'Submit a pokemon'
-  } else if (status === 'pending') {
-    return <PokemonInfoFallback name={pokemonName} />
-  } else if (status === 'rejected') {
+  if (error) {
     return (
       <div role="alert">
         There was an error:{' '}
         <pre style={{whiteSpace: 'normal'}}>{error.message}</pre>
       </div>
     )
-  } else if (status === 'resolved') {
-    return <PokemonDataView pokemon={pokemon} />
   }
 
-  throw new Error('Team Rocket, blast off a the speed of light')
+  if (!pokemonName) {
+    return 'Submit a pokemon'
+  } else if (!pokemon) {
+    return <PokemonInfoFallback name={pokemonName} />
+  } else {
+    return <PokemonDataView pokemon={pokemon} />
+  }
 }
 
 function App() {
