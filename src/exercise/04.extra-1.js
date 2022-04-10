e8dc4582b9aaedaebb9c0a4cc1af6fe619147697
Ryan Apellanes <ryanapellanes.dev@gmail.com>
2022-04-09 22:56:46 -0700

    extra 1,2,3,4

--- src/exercise/04.extra-1.js
+++ src/exercise/04.extra-1.js
@@ -4,7 +4,6 @@
 import * as React from 'react'
 
 function Board() {
-  // 🐨 squares is the state for this component. Add useState for squares
   const [squares, setSquares] = React.useState(
     () =>
       JSON.parse(window.localStorage.getItem('squares')) || Array(9).fill(null),
@@ -14,34 +13,11 @@ function Board() {
     window.localStorage.setItem('squares', JSON.stringify(squares))
   }, [squares])
 
-  // 🐨 We'll need the following bits of derived state:
-  // - nextValue ('X' or 'O')
-  // - winner ('X', 'O', or null)
-  // - status (`Winner: ${winner}`, `Scratch: Cat's game`, or `Next player: ${nextValue}`)
-  // 💰 I've written the calculations for you! So you can use my utilities
-  // below to create these variables
   const nextValue = calculateNextValue(squares)
   const winner = calculateWinner(squares)
   const status = calculateStatus(winner, squares, nextValue)
 
-  // This is the function your square click handler will call. `square` should
-  // be an index. So if they click the center square, this will be `4`.
   function selectSquare(square) {
-    // 🐨 first, if there's already winner or there's already a value at the
-    // given square index (like someone clicked a square that's already been
-    // clicked), then return early so we don't make any state changes
-    //
-    // 🦉 It's typically a bad idea to mutate or directly change state in React.
-    // Doing so can lead to subtle bugs that can easily slip into production.
-    //
-    // 🐨 make a copy of the squares array
-    // 💰 `[...squares]` will do it!)
-    //
-    // 🐨 set the value of the square that was selected
-    // 💰 `squaresCopy[square] = nextValue`
-    //
-    // 🐨 set the squares to your copy
-
     if (winner || squares[square]) {
       return
     }
@@ -51,8 +27,6 @@ function Board() {
   }
 
   function restart() {
-    // 🐨 reset the squares
-    // 💰 `Array(9).fill(null)` will do it!
     setSquares(Array(9).fill(null))
   }
 
